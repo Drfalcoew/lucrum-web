ce47704ffb1e10027c29451a09cae694b5c02fcd
Drfalcoew <drfalcoew@gmail.com>
2024-03-15 10:52:36 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img className='Background-image' src='ethereal_0.webp' alt='google logo' />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
