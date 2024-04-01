1e503e4654c374a70900a0c303e1676593a27bcd
Drfalcoew <drfalcoew@gmail.com>
2024-04-01 10:59:09 -0700

    get-started page top finished

--- src/main/pages/get-started/GetStarted.tsx
+++ src/main/pages/get-started/GetStarted.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
 import '../Page.css';
 import './GetStarted.css';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Button } from 'antd';
 
 const GetStarted = () => {
 
     const cards = [
         {
             title: 'Standard',
-            description: 'Get started with Lucrum today!',
-            icon: 'laurel.png'
+            description: `Unleash the Power of Your Brand Online and Offline!`,
+            price: '$349.00',
+            details: (
+                <ul>
+                    <li>Website landing page</li>
+                    <li>Video shorts/reels</li>
+                    <li>Traditional advertising (flyers, posters)</li>
+                    <li>Social media management</li>
+                </ul>
+            )
         },
         {
             title: 'Premium',
-            description: 'Get started with Lucrum today!',
-            icon: 'laurel.png'
+            description: 'Elevate Your Brand\'s Presence with Premium Marketing Solutions!',
+            price: '$499.00',
+            details: (
+                <ul>
+                    <li>All features of Standard</li>
+                    <li>Comprehensive website with SEO</li>
+                    <li>Engaging commercials and videos</li>
+                    <li>AI integration</li>
+                </ul>
+            )
         },
         {
             title: 'Deluxe',
-            description: 'Get started with Lucrum today!',
-            icon: 'laurel.png'
+            description: 'Experience Marketing Excellence at Its Finest!',
+            price: '$699.00',
+            icon: 'lightning.gif',
+            details: (
+                <ul>
+                    <li>All features of Premium</li>
+                    <li>Advanced AI integration</li>
+                    <li>Extended social media management</li>
+                    <li>Advanced analytics</li>
+                    <li>Custom marketing solutions</li>
+                </ul>
+            )
         }
     ];
     
@@ -33,12 +59,24 @@ const GetStarted = () => {
                             <Col span={8} key={index}>
                                 <div className='custom-card-root-container'>
                                     <Card
-                                        title={card.title}
-                                        className='getstarted-card-container'
+                                        style={{ backgroundColor: index % 2 === 1 ? 'rgba(35,35,35, 0.8)' : 'rgba(49,49,49, 0.8)' }}
+                                        className='getstarted-card'
                                     >
                                         <Card.Meta
-                                            description={card.description}
+                                            title={<div className='getstarted-card-title'>{card.title}</div>}
+                                            avatar={<img className='getstarted-card-icon' alt='' src={card.icon} />}
                                         />
+                                        <div className='getstarted-card-description'>{card.description}</div>
+                                        <div className='getstarted-card-content'>
+                                            <div className='getstarted-price-container'>
+                                                <div className='getstarted-card-price'>{card.price}</div>
+                                                <div className='getstarted-card-price-details'>per month</div>
+                                            </div>
+                                            <div className='getstarted-card-details'>
+                                                {card.details}
+                                            </div>
+                                            <Button type='primary' className='getstarted-card-button'>Get Started</Button>
+                                        </div>
                                     </Card>
                                 </div>
                             </Col>
